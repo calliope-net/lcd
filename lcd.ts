37b86a132e84a725ea3d7957ac1a41475a7beb31
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-10-07 10:46:09 +0200

    Aktualisiere lcd.ts

--- lcd.ts
+++ lcd.ts
@@ -32,20 +32,41 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
         else if (q_display == eDisplay.qwiic_16_2) { q_i2c = 0x72; q_rows = 2; q_cols = 16 }
         else if (q_display == eDisplay.qwiic_20_4) { q_i2c = 0x72; q_rows = 4; q_cols = 20 }
 
-        // LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF // 0x0C
-        // set_display(true, false, false)
-        if (i2c_check && !special_command(LCD_DISPLAYCONTROL + 0x04)) // Display on, Corsor off
-            q_display = eDisplay.none
+        if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4) {
+            // LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF // 0x0C
+            // set_display(true, false, false)
+            if (i2c_check && !special_command(LCD_DISPLAYCONTROL + 0x04)) // Display on, Corsor off
+                q_display = eDisplay.none
 
-        // LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT // 0x06
-        //entrymodeset(pADDR, eLCD_ENTRYMODE.LCD_ENTRYLEFT, eLCD_ENTRYSHIFT.LCD_ENTRYSHIFTDECREMENT)
-        if (reset) {
-            // RGB white
-            //setBacklight(pADDR, 255, 255, 255)
-            // CONTRAST_COMMAND
-            setting_command_2(eSETTING_COMMAND_2.CONTRAST_COMMAND, 0)
+            // LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT // 0x06
+            //entrymodeset(pADDR, eLCD_ENTRYMODE.LCD_ENTRYLEFT, eLCD_ENTRYSHIFT.LCD_ENTRYSHIFTDECREMENT)
+            if (reset) {
+                // RGB white
+                //setBacklight(pADDR, 255, 255, 255)
+                // CONTRAST_COMMAND
+                setting_command_2(eSETTING_COMMAND_2.CONTRAST_COMMAND, 0)
+            }
+            clear_display()
         }
-        clear_display()
+
+        else if (q_display == eDisplay.grove_16_2) {
+            control.waitMicros(30000) // Power on + more than 15ms
+
+            //write0x80Byte(0x38) // Function Set DL N
+            if (i2c_check && !write0x80Byte(0x38)) // Display on, Cursor off
+                q_display = eDisplay.none
+
+            control.waitMicros(50) // >39µs
+            //if (i2cNoError(pADDR)) {
+            write0x80Byte(0x0C) // Display ON, Cursor OFF
+            control.waitMicros(50) // >39µs
+            write0x80Byte(0x01) // Screen Clear
+            control.waitMicros(1600) // > 1.53ms
+            write0x80Byte(0x06) // Increment Mode
+            control.waitMicros(50) // >39µs
+            //}
+            //control.waitMicros(30000)
+        }
     }
 
     export enum eLCD_CLEARDISPLAY {
@@ -58,9 +79,13 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
     //% group="LCD Display"
     //% block="Display löschen" weight=5
     export function clear_display() {
-        if (q_display != eDisplay.none) {
+        if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4) {
             special_command(eLCD_CLEARDISPLAY.LCD_CLEARDISPLAY)
             set_cursor(0, 0)
+        }
+        else if (q_display == eDisplay.grove_16_2) {
+            write0x80Byte(0x01)
+            control.waitMicros(1600)
         }
     }
 
@@ -113,17 +138,46 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
                     text = "                    ".substr(0, len - text.length) + text
                 else if (text.length < len)
                     text = text + "                    ".substr(0, len - text.length)
-
-                if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4)
+                /*
+                let bu: Buffer
+                if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4) {
                     text = text.replace("|", "||")
-                let bu = Buffer.create(text.length)
-                for (let i = 0; i < text.length; i++) {
-                    bu.setUint8(i, change_char_code(text, i))
+                    bu = Buffer.create(text.length)
+                    for (let i = 0; i < text.length; i++) {
+                        bu.setUint8(i, change_char_code(text, i))
+                    }
                 }
-                i2c_write_buffer(bu)
+                else if (q_display == eDisplay.grove_16_2) {
+                    bu = Buffer.create(text.length + 1)
+                    bu.setUint8(0, 0x40)
+                    for (let i = 0; i <= text.length - 1; i++) {
+                        bu.setUint8(i + 1, change_char_code(text, i))
+                    }
+                }
+                */
+                i2c_write_buffer(write_text_to_buffer(text))
                 basic.pause(10) // sleep(0.01)
             }
         }
+    }
+
+    function write_text_to_buffer(text: string) {
+        let buffer: Buffer
+        if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4) {
+            text = text.replace("|", "||")
+            buffer = Buffer.create(text.length)
+            for (let i = 0; i < text.length; i++) {
+                buffer.setUint8(i, change_char_code(text, i))
+            }
+        }
+        else if (q_display == eDisplay.grove_16_2) {
+            buffer = Buffer.create(text.length + 1)
+            buffer.setUint8(0, 0x40)
+            for (let i = 0; i <= text.length - 1; i++) {
+                buffer.setUint8(i + 1, change_char_code(text, i))
+            }
+        }
+        return buffer
     }
 
 
@@ -144,15 +198,20 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
             let text: string = convertToText(value)
             if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4)
                 text = text.replace("|", "||")
-            let bu = Buffer.create(text.length)
-            for (let i = 0; i < text.length; i++) {
-                bu.setUint8(i, change_char_code(text, i))
+
+            let buffer = write_text_to_buffer(text)
+
+            if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.qwiic_20_4) {
+                // 22.09.2025 Qwiic LCD Display erlaubt nur max 32 Byte pro Buffer; für 80 Zeichen teilen in 3 Buffer 3*27 = 81
+                // Zeichen werden auf nächster Zeile weiter geschrieben, nach Ende des Displays Fortsetzung auf erster Zeile
+                let bu_list = buffer.chunked(27) // Splits buffer into parts no longer than 27
+                for (let i = 0; i < bu_list.length; i++) {
+                    i2c_write_buffer(bu_list[i])
+                    basic.pause(10) // sleep(0.01)
+                }
             }
-            // 22.09.2025 Qwiic LCD Display erlaubt nur max 32 Byte pro Buffer; für 80 Zeichen teilen in 3 Buffer 3*27 = 81
-            // Zeichen werden auf nächster Zeile weiter geschrieben, nach Ende des Displays Fortsetzung auf erster Zeile
-            let bu_list = bu.chunked(27) // Splits buffer into parts no longer than 27
-            for (let i = 0; i < bu_list.length; i++) {
-                i2c_write_buffer(bu_list[i])
+            else if (q_display == eDisplay.grove_16_2) {
+                i2c_write_buffer(buffer)
                 basic.pause(10) // sleep(0.01)
             }
         }
@@ -173,16 +232,22 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
     //% increment.shadow=toggleYesNo
     //% inlineInputMode=inline
     export function write_array(text_list: string[], increment: eINC, list_index?: number, string_index?: number) {
+        // zeigt 60 Zeichen aus text_list an und schaltet weiter
+        let zeichen: number
 
         if (q_display != eDisplay.none && text_list.length > 0) {
-            // zeigt 60 Zeichen aus text_list an und schaltet weiter
-            if (list_index !== undefined) { q_list_index = list_index } else { }
+            if (q_display == eDisplay.qwiic_20_4)
+                zeichen = 60
+            else if (q_display == eDisplay.qwiic_16_2 || q_display == eDisplay.grove_16_2)
+                zeichen = 16
+
+            if (list_index !== undefined) { q_list_index = list_index }
             if (string_index !== undefined) { q_string_index = string_index }
             if (!between(q_list_index, 0, text_list.length - 1)) { q_list_index = 0 }
             let text: string = text_list[q_list_index]
             if (!between(q_string_index, 0, text.length - 1)) { q_string_index = 0 }
 
-            let text_substr = text.substr(q_string_index, 60)
+            let text_substr = text.substr(q_string_index, zeichen)
 
             clear_display()
             // Zeile 0 text_list_index, text_substr_index, text_length(gesamt)
@@ -190,16 +255,17 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
                 q_list_index + "/" + text_list.length + " " + q_string_index + "-" + (q_string_index + Math.min(59, text_substr.length - 1)) + "/" + text.length)
 
             // Zeile 1-2-3 60 Zeichen von text = 3 Zeilen x 20 Zeichen
+            // Zeile 1 16 Zeichen von text = 1 Zeilen x 16 Zeichen
             set_cursor(1, 0)
             write_lcd(text_substr)
 
             if (increment == eINC.inc1) {
-                if (text.length > q_string_index + 60) {
-                    q_string_index += 60
+                if (text.length > q_string_index + zeichen) {
+                    q_string_index += zeichen // 60 oder 16
                 }
                 else if (text_list.length - 1 > q_list_index) {
-                    q_list_index += 1
-                    q_string_index = 0
+                    q_list_index += 1  // nächstes Element im String-Array
+                    q_string_index = 0 // String auf Anfang
                 }
                 else {
                     q_list_index = 0
@@ -246,7 +312,11 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
     //% blockId=lcd_text block="%s" blockHidden=true
     export function lcd_text(s: string): string { return s }
 
+    // Qwiic 0xFE = 254
     function special_command(command: number) { return i2c_write_buffer(Buffer.fromArray([SPECIAL_COMMAND, command & 0xFF])) }
+
+    // Grove 0x80 = 128
+    function write0x80Byte(command: number) { return i2c_write_buffer(Buffer.fromArray([0x80, command])) }
 
     //function sleep(sekunden: number) { basic.pause(sekunden * 1000) }
 
@@ -295,6 +365,7 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
         return text.charCodeAt(i) & 0xFF // es können nur 1 Byte Zeichen-Codes im Buffer übertragen werden
     }
 
+    // Qwiic
     // OpenLCD command characters
     const SPECIAL_COMMAND = 254  // Magic number for sending a special command
     const SETTING_COMMAND = 0x7C // 124, |, the pipe character: The command to change settings: baud, lines, width, backlight, splash, etc
@@ -310,6 +381,10 @@ Lutz Elßner, Freiberg, Oktober 2025, lutz@elssner.net
     //    LCD_SETCGRAMADDR = 0x40   nicht benutzt
     // set Cursor
     const LCD_SETDDRAMADDR = 0x80   // SPECIAL_COMMAND, LCD_SETDDRAMADDR | (col + row_offsets[row])
+
+
+
+    // Grove
 
 
 } // lcd.ts
